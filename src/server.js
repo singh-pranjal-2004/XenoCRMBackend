be28d1982442775eaa1775f7d75ad22b9f2cf591
Pranjal Ashok Singh <pranjalsingh.ak@gmail.com>
2025-05-13 20:31:00 +0530

    cors edited

--- src/server.js
+++ src/server.js
@@ -16,8 +16,18 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://your-netlify-site.netlify.app' // ← update this
+];
+
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
